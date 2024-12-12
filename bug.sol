f4458e1c6f4e87ecd9b8950de737492c9b67ff3e
BugHunter-X <askzenai@gmail.com>
2024-12-12 19:32:05 +0100

    Add bug.sol file

---
+++ bug.sol
@@ -0,0 +1,10 @@
+function checkIfAddressIsRegistered(address) {
+  // ... some code to check if address is registered ...
+  if (isRegistered) {
+    return true; 
+  } else {
+    // MISSING: Handle the case where the address is NOT registered.
+    // This might throw an error or return an unexpected value
+    return false;
+  }
+}
